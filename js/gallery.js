071f63f4462c0bd8bd5c54725eda730483cc10d4
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-02 11:15:10 -0500

    completed image gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,36 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
+
+
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img"); //solutiuon
+    let mainImage = document.querySelector("#gallery-photo > img"); 
+
+      // Preload large images.
+      let newImageSrc  = thumbnail.dataset.largeVersion;
+      let largeVersion = new Image();
+      largeVersion.src = newImageSrc;
+
+    thumbnails.forEach(function(thumbnail){
+        thumbnail.addEventListener("click", function(){
+            //set clicked image as main image
+            mainImage.setAttribute("src", newImageSrc);
+            mainImage.setAttribute('alt', thumbnail.alt);
+
+            //change which image is current
+            let currentClass = "current";
+            document.querySelector(".current").classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+
+            //update image info 
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title       = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
   }
