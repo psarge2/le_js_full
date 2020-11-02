b0ce89f2852ae478289e6789042617705275fb32
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-02 14:02:29 -0500

    reverted back to functional

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,14 @@
 // Activates the image gallery.
 // The main task is to attach an event listener to each image in the gallery
 // and respond appropriately on click.
-
-
-
 function activateGallery() {
     let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img"); //solutiuon
     let mainImage = document.querySelector("#gallery-photo > img"); 
 
-      // Preload large images.
-      let newImageSrc  = thumbnail.dataset.largeVersion;
-      let largeVersion = new Image();
-      largeVersion.src = newImageSrc;
-
     thumbnails.forEach(function(thumbnail){
         thumbnail.addEventListener("click", function(){
             //set clicked image as main image
+            let newImageSrc  = thumbnail.dataset.largeVersion;
             mainImage.setAttribute("src", newImageSrc);
             mainImage.setAttribute('alt', thumbnail.alt);
 
